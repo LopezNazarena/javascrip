9548a1623b0d86ba67920bca72330b96000511ca
Lopez Nazarena <nazarenalopez40@gmail.com>
2022-07-04 15:34:43 -0300

    se realizo alert, suma de numeros, trabajo con prompt y con parseInt

---
+++ js/variables.js
@@ -0,0 +1,12 @@
+// alert("Bienvenidos");
+
+document.write("<h1>Hello word</h1>");
+
+document.write(3 + 5);
+
+var usuario = prompt("Hola ¿cual es tu nombre?");
+document.write("<p>Hola nombreUsuario</p>")
+
+var numero= prompt("Escribe un numero")
+var numero2= prompt("Escribe nuevamente un numero")
+document.write("La suma es: "+ (parseInt(numero) + parseInt(numero2)))
