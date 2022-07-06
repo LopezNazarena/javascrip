328bd6cf059d16697e3a9de09c1a7aee3f942024
Lopez Nazarena <nazarenalopez40@gmail.com>
2022-07-06 17:48:56 -0300

    agregue los ultimos 3 ejercicios del trabajo practico

--- js/variables.js
+++ js/variables.js
@@ -19,7 +19,7 @@ document.write(3 + 5);
 // if(numero3 > numero4) document.write("El primer numero es el mayor numero ingresado: " + numero3);
 // else if(numero4 > numero3){
 //  document.write("El segundo numero es el mayor, numero ingresado " + numero4);
-// } 
+// }
 
 // // punto 7
 // let numero5= prompt("Escribe un numero")
@@ -32,10 +32,63 @@ document.write(3 + 5);
 //     document.write("El tercer numero es el mayor, el numero es " + numero7);
 // }
 
-// punto 8
-let numero= prompt("Introduzca un numero")
-if(numero % 2 === 0){
-document.write("El numero es divisible en 2 ")
-} else{
-    document.write("El numero no es divisible por 2")
+// // punto 8
+// let numero= prompt("Introduzca un numero")
+// if(numero % 2 === 0){
+// document.write("El numero es divisible en 2 ")
+// } else{
+//     document.write("El numero no es divisible por 2")
+// }
+
+// punto 9
+let frase = prompt("Ingrese una frase")
+console.log(frase)
+console.log(frase.length)
+
+if(frase.charAt(0) === "a" || frase.charAt(0) ==="e" || frase.charAt(0) === "i" ||frase.charAt(0) ==="o" || frase.charAt(0) === "u"){
+    document.write(frase.charAt(0))
 }
+
+if(frase.charAt(1) === "a" || frase.charAt(1) ==="e" || frase.charAt(1) === "i" ||frase.charAt(1) ==="o" || frase.charAt(1) === "u"){
+    document.write(frase.charAt(1))
+}
+
+if(frase.charAt(2) === "a" || frase.charAt(2) ==="e" || frase.charAt(2) === "i" ||frase.charAt(2) ==="o" || frase.charAt(2) === "u"){
+    document.write(frase.charAt(2))
+}
+
+if(frase.charAt(0) === "a" || frase.charAt(3) ==="e" || frase.charAt(3) === "i" ||frase.charAt(3) ==="o" || frase.charAt(3) === "u"){
+    document.write(frase.charAt(3))
+}
+
+if(frase.charAt(0) === "a" || frase.charAt(4) ==="e" || frase.charAt(4) === "i" ||frase.charAt(4) ==="o" || frase.charAt(4) === "u"){
+    document.write(frase.charAt(4))
+}
+
+// // // punto 10
+// let numero= prompt("Introduzca un numero")
+// if(numero % 2 === 0){
+// document.write("El numero es divisible en 2")
+// } else if (numero % 3 === 0) {
+//     document.write("El numero es divisible por 3");
+// }  else if (numero % 5 === 0) {
+//     document.write("El numero es divisible por 5");
+// }  else if (numero % 7 === 0) {
+//     document.write("El numero es divisible por 7");
+// }
+
+
+// // punto 11
+// let numero0 = prompt("Introduzca un numero");
+// if (numero0 % 2 === 0) {
+//   document.write("El numero es divisible en 2");
+// }
+// if (numero0 % 3 === 0) {
+//   document.write("El numero es divisible por 3");
+// }
+// if (numero0 % 5 === 0) {
+//   document.write("El numero es divisible por 5");
+// }
+// if (numero0 % 7 === 0) {
+//   document.write("El numero es divisible por 7");
+// }
